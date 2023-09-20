57edaf192583fc077f336b83ccd8b1f678ccefad
MihailOP101 <mishoisapro@gmail.com>
2023-09-20 16:12:57 -0400

    Initial Commit

---
+++ main/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  main
+//
+//  Created by Mihail Karamanolev on 9/20/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
